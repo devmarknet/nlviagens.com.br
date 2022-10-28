4fd9a8522752ff2f5e6c5d3692e117c87b9c19cd
devmarknet <frontend.marknet@gmail.com>
2022-10-28 17:47:29 -0300

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'HeaderComponent',
+    props: {
+    headerProps: { type: Object },
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title">{{ headerProps.main_section.title }}</h1>
+    <p class="sub-title" v-html="headerProps.main_section.sub_title" />
+    <a class="action" :href="headerProps.main_section.action">{{ headerProps.main_section.action_text }}</a>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.container {
+  .title {
+    @apply
+      text-7xl
+    ;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<script>
+import { gql } from 'nuxt-graphql-request'
+
+export default {
+  name: 'IndexPage',
+  async asyncData({ $graphql }) {
+     const query = gql`
+        query {
+          main_section {
+            title
+            sub_title
+            action_text
+            action
+            file
+            status
+          }
+        }
+      `
+    const data = await $graphql.admin.request(query)
+    return {
+      data
+    }
+  }
+
+}
+</script>
+
+<template>
+  <div>
+    <Header :headerProps="data"/>
+  </div>
+</template>
