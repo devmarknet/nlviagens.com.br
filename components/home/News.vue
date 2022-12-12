48b0bcf9b8a7dd44198d500aff4ca152a7453b8a
Rodrigo Andrade <rodrigo@marknet.com.br>
2022-12-12 17:33:16 -0300

    form newsletter

--- components/home/News.vue
+++ components/home/News.vue
@@ -6,12 +6,71 @@ export default {
 
 <template>
   <div>
-    <div class="container mx-auto max-w-7xl">
+    <div class="container mx-auto max-w-7xl flex-col">
       <div class="box-content">
         <span class="span">Notícias</span>
         <h2 class="title max-w-3xl">
           As últimas notícias nacionais e internacionais no mundo do turismo
         </h2>
+        <div class="w-full grid grid-cols-4 gap-8 py-10">
+          <NuxtLink to="/">
+            <img
+              class="flex-1 rounded-lg overflow-hidden"
+              :src="require('~/assets/images/tour-intercontinental.jpeg')"
+              alt=""
+            />
+          </NuxtLink>
+          <NuxtLink to="/">
+            <img
+              class="flex-1 rounded-lg overflow-hidden"
+              :src="require('~/assets/images/ferias-em-familia.jpeg')"
+              alt=""
+            />
+          </NuxtLink>
+          <NuxtLink to="/">
+            <img
+              class="flex-1 rounded-lg overflow-hidden"
+              :src="require('~/assets/images/ferias-na-franca.jpeg')"
+              alt=""
+            />
+          </NuxtLink>
+          <NuxtLink to="/">
+            <img
+              class="flex-1 rounded-lg overflow-hidden hover:object-fill"
+              :src="require('~/assets/images/passagens-internacionais.jpeg')"
+              alt=""
+            />
+          </NuxtLink>
+        </div>
+        <NuxtLink class="button" to="/promocoes">Veja mais notícias</NuxtLink>
+      </div>
+      <div class="box-content mt-32">
+        <span class="span">Newsletter</span>
+        <h2 class="title max-w-3xl">
+          Receba as novidades em primeira mão assinando nossa newsletter
+        </h2>
+        <div class="w-full flex flex-col gap-8 mt-8">
+          <form class="flex gap-3 items-center" action="">
+            <input
+              class="py-3 px-6 rounded-lg outline-none"
+              type="text"
+              placeholder="Seu nome completo"
+            />
+            <input
+              class="py-3 px-6 rounded-lg outline-none"
+              type="text"
+              placeholder="Seu melhor e-mail"
+            />
+            <button
+              class="bg-blue text-white py-3 px-6 rounded-lg outline-none"
+            >
+              Quero receber novidades
+            </button>
+          </form>
+          <p class="text-sm text-gray">
+            * Ao inserir seus dados você aceita receber comunicações.
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -19,7 +78,7 @@ export default {
 
 <style lang="postcss" scoped>
 .container {
-  @apply bg-neutral-200
+  @apply bg-neutral-100
       flex
       py-32
       min-w-full
