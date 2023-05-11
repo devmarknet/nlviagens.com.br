ec8cf2612c06162064645285d43de172614b5692
rodriggomk <101878677+rodriggomk@users.noreply.github.com>
2023-05-11 08:34:29 -0300

    facebookscript

--- layouts/web.vue
+++ layouts/web.vue
@@ -1,8 +1,26 @@
 <script>
 import NavBar from '@/components/NavBar.vue'
 import FooterLayout from '@/components/Footer.vue'
+
+function facebookScript () {
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '758928589222147');
+  fbq('track', 'PageView');
+}
+
 export default {
   name: 'webLayout',
+  head() {
+    facebookScript()
+    return {}
+  },
   components: { NavBar, FooterLayout },
 }
 </script>
